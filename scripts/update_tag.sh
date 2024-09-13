6d0a5bed57bcbe394457fc53c4ca3068e206eaaa
Adam Pavlat <adam.pavlat>
2024-09-13 10:03:03 +0200

    add permissions to workflow

--- scripts/update_tag.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-
-# Get the current Git tag
-GIT_TAG=$(git describe --tags --abbrev=0)
-
-# Replace the placeholder {{GIT_TAG}} in index.html with the actual tag
-sed -i "s/{{GIT_TAG}}/$GIT_TAG/" nginx/index.html
