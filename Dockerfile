5b99ec7f9eb075325e68525ba77b27f3ec37903b
Adam Pavlat <adam.pavlat>
2024-09-13 14:28:46 +0200

    add build json

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,9 @@
 # Start from the official nginx alpine image
 FROM nginx:1.27.1-alpine
 
-# Copy the custom index.html to the default nginx HTML location
+# Copy the custom index.html and info.json to the default nginx HTML location
 COPY nginx/index.html /usr/share/nginx/html/index.html
+COPY nginx/info.json /usr/share/nginx/html/info.json
 
 # Expose port 80
 EXPOSE 80
--- scripts/update_commit_info.sh
+++ scripts/update_commit_info.sh
@@ -4,17 +4,24 @@ set -ex
 # Get the latest commit hash
 COMMIT_HASH=$(git rev-parse --short HEAD)
 
-# Get the current timestamp in a human-readable format
-BUILD_TIME=$(date -u +"%Y-%m-%d %H:%M:%S UTC")
+# Get the current timestamp adjusted to UTC+2
+BUILD_TIME=$(date -u -d '+2 hour' +"%Y-%m-%d %H:%M:%S UTC+2")
 
 # Replace the placeholders {{COMMIT_HASH}} and {{BUILD_TIME}} in index.html
-# This version of `sed` works both on Linux and macOS
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  # For macOS, use the `-i ''` argument to avoid issues with sed in-place editing
+  # For macOS
   sed -i '' "s/{{COMMIT_HASH}}/$COMMIT_HASH/" nginx/index.html
   sed -i '' "s/{{BUILD_TIME}}/$BUILD_TIME/" nginx/index.html
 else
-  # For Linux, the `-i` argument works fine
+  # For Linux
   sed -i "s/{{COMMIT_HASH}}/$COMMIT_HASH/" nginx/index.html
   sed -i "s/{{BUILD_TIME}}/$BUILD_TIME/" nginx/index.html
 fi
+
+# Generate the info.json file
+cat > nginx/info.json << EOF
+{
+  "commit_hash": "$COMMIT_HASH",
+  "build_time": "$BUILD_TIME"
+}
+EOF
